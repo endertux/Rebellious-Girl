7a5549559420aae1a447757990f131e354cdc5da
Helwa <hhalloum879@gmail.com>
2024-03-18 19:32:15 -0700

    attempt to fix anims frames

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -5,13 +5,13 @@ class Load extends Phaser.Scene {
 
     preload() {
         //load images
-        this.load.image('playBackground', 'assets/play_bg.png');
-        this.load.image('howtoplayBG', 'assets/how_to_play.png');
-        this.load.image('creditsBG', 'assets/credits_bg.png');
-        this.load.image('menuBackground', 'assets/menu_screen.png');
-        this.load.image('gameover', 'assets/gameover_title.png');
-        this.load.image('clam', 'assets/clam.png');
-        this.load.image('banana', 'assets/bannana.png');
+        this.load.image('playBackground', './assets/play_bg.png');
+        this.load.image('howtoplayBG', './assets/how_to_play.png');
+        this.load.image('creditsBG', './assets/credits_bg.png');
+        this.load.image('menuBackground', './assets/menu_screen.png');
+        this.load.image('gameover', './assets/gameover_title.png');
+        this.load.image('clam', './assets/clam.png');
+        this.load.image('banana', './assets/bannana.png');
         
         // miku punch anims
         this.load.spritesheet('mikuPunch', './assets/Miku_anims.png', {
@@ -20,13 +20,13 @@ class Load extends Phaser.Scene {
         })
         
         // miku anims for game over screen
-        this.load.spritesheet('sadMiku', 'assets/miku_anims_sad.png', {
+        this.load.spritesheet('sadMiku', './assets/miku_anims_sad.png', {
             frameWidth: 462,
             frameHeight: 510
         })
 
         // miku punching up anims
-        this.load.spritesheet('mikuUP', 'assets/miku_anims_up.png', {
+        this.load.spritesheet('mikuUP', './assets/miku_anims_up.png', {
             frameWidth: 460,
             frameHeight: 613
         })
@@ -36,7 +36,7 @@ class Load extends Phaser.Scene {
         // https://pixabay.com/music/cartoons-anime-beginings-139797/
         this.load.audio('bgMusic', './assets/SFX/anime-beginings-139797.mp3')
         // audio from Hatsune Miku voice bank
-        this.load.audio('ouch', 'assets/SFX/misstake-101soundboards.mp3')
+        this.load.audio('ouch', './assets/SFX/misstake-101soundboards.mp3')
         
     }
 
